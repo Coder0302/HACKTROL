0559a3ecaf282ea19247b7813ce214e562483b53
FokinDanil <39062072+Danil123456789@users.noreply.github.com>
2021-10-26 01:42:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 print("Hallo")
 print(os.environ.get('PORT'))
-server_address = ("https://hacktrol.herokuapp.com/", os.environ.get('PORT'))
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
